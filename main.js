994ce012e6ab2edb28fd72e5b34e47f388d3dea7
pavelbelozerov <thecalifornia@ya.ru>
2021-05-28 22:54:07 +0300

    урок второй, типы данных, операторы, методы и свойства

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 
-let money = 50 ;
-let income;
-let addExpenses;
-let deposit;
-let mission; 
-let period;
+let money = 10000 ;
+let income = 'frilance';
+let addExpenses = 'Taxi, Rental, Call';
+let deposit = true;
+let mission = 99999999990; 
+let period = 7;
 
-alert('Hello, world');
+console.log("money: ", money);
+console.log("income: ", income);
+console.log("deposit: ", deposit);
+console.log(addExpenses.length);
+console.log('период равен ' + period + ' месяцев' );
+console.log('Цель заработать ' + mission + ' долларов' );
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split());
 
-console.log(money);
+let budgetDay = 1200;
+console.log('budgetDay: ', budgetDay/30);
+
+
+
